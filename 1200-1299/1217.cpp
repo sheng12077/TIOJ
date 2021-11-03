4ba97d9b163f58428930c39bc2a75a9f298a8fac
sheng <86102390+sheng120777@users.noreply.github.com>
2021-11-03 14:48:25 +0800

    Create 1217.cpp

---
+++ 1200-1299/1217.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+#define inf 1e9
+#define maxn 10000005
+
+signed main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s;
+    while(getline(cin,s)){
+        cout<<s<<endl;
+    }
+}
